fed2ae5e0b2dd245e26cebc69645f5723b978560
Guilherme Brogio Macedo da Silva <guilhermebrogio.ps@gmail.com>
2023-06-15 16:46:58 -0300

    Feats

--- src/components/Map/Sidebar/index.tsx
+++ src/components/Map/Sidebar/index.tsx
@@ -28,18 +28,13 @@ export const Sidebar = ({
   };
 
   return (
-    <div
-      className={clsx(
-        'absolute top-0 left-0 h-full transition-all lg:translate-x-0 -translate-x-full',
-        { 'translate-x-0': sidebarOpen },
-      )}
-    >
+    <>
       <button
         className={clsx(
-          'lg:hidden absolute right-0 w-fit flex top-0 m-4 p-2 text-left rounded-sm bg-gray-200 text-gray-600',
+          'lg:hidden absolute left-[-6px] w-fit z-50 flex top-0 m-4 p-2 text-left rounded-sm bg-gray-200 text-gray-600',
           {
-            'top-10 bg-white w-[calc(100vw-10%)] max-w-max right-unset left-[calc(100%-6px)]':
-              !sidebarOpen,
+            'left-full -translate-x-[calc(100%+3rem)]': sidebarOpen,
+            'top-10 bg-white w-[calc(100vw-10%)] max-w-max': !sidebarOpen,
           },
         )}
         aria-label="Toggle Sidebar"
@@ -53,64 +48,71 @@ export const Sidebar = ({
           <span className="w-full flex">Conheça mais sobre o distribuidor</span>
         )}
       </button>
-      <div className="custom-scrollbar left-0 min-w-80 py-8 lg:py-4 overflow-y-auto px-4 h-full bg-white shadow-sm">
-        <div className="pb-8 px-4">
-          {postalCodeSubmitted && (
-            <button
-              className="bg-gray-300 flex items-center justify-center gap-2 w-fit p-1 px-4 rounded-full hover:bg-red-300"
-              onClick={() => {
-                setPostalCodeSubmitted(undefined);
-                setMarkerIds([]);
-              }}
-            >
-              {postalCodeSubmitted}
-              <Cross1Icon className="w-3 h-3" />
-            </button>
-          )}
-        </div>
-
-        {!hasError &&
-          locatesInfo
-            .filter((locateInfo) =>
-              markerIds.length ? markerIds.includes(locateInfo.id) : true,
-            )
-            .map(({ imageUrl, desc, id, locale, postalCode, ...coordinates }) => (
-              <div
-                className="flex flex-col mb-4 group overflow-hidden gap-2 shadow-md select-none cursor-pointer bg-gray-200 rounded-lg transition"
-                key={id}
+      <div
+        className={clsx(
+          'absolute top-0 left-0 h-full transition-all lg:translate-x-0 -translate-x-full',
+          { 'translate-x-0': sidebarOpen },
+        )}
+      >
+        <div className="custom-scrollbar left-0 min-w-80 py-8 lg:py-4 overflow-y-auto px-4 h-full bg-white shadow-sm">
+          <div className="pb-8 px-4">
+            {postalCodeSubmitted && (
+              <button
+                className="bg-gray-300 flex items-center justify-center gap-2 w-fit p-1 px-4 rounded-full hover:bg-red-300"
                 onClick={() => {
-                  setLatitude(coordinates.lat);
-                  setLongitude(coordinates.lon);
-                  setPostalCodeSubmitted(postalCode);
-                  setMarkerIds([id]);
+                  setPostalCodeSubmitted(undefined);
+                  setMarkerIds([]);
                 }}
               >
-                {imageUrl?.length ? (
-                  <div className="w-full min-h-[8rem] block overflow-hidden">
-                    <Image
-                      alt=""
-                      src={imageUrl}
-                      width={2080}
-                      height={2080}
-                      className="w-full h-full block group-hover:scale-110 transition object-cover object-center"
-                    />
+                {postalCodeSubmitted}
+                <Cross1Icon className="w-3 h-3" />
+              </button>
+            )}
+          </div>
+
+          {!hasError &&
+            locatesInfo
+              .filter((locateInfo) =>
+                markerIds.length ? markerIds.includes(locateInfo.id) : true,
+              )
+              .map(({ imageUrl, desc, id, locale, postalCode, ...coordinates }) => (
+                <div
+                  className="flex flex-col mb-4 group overflow-hidden gap-2 shadow-md select-none cursor-pointer bg-gray-200 rounded-lg transition"
+                  key={id}
+                  onClick={() => {
+                    setLatitude(coordinates.lat);
+                    setLongitude(coordinates.lon);
+                    setPostalCodeSubmitted(postalCode);
+                    setMarkerIds([id]);
+                  }}
+                >
+                  {imageUrl?.length ? (
+                    <div className="w-full min-h-[8rem] block overflow-hidden">
+                      <Image
+                        alt=""
+                        src={imageUrl}
+                        width={2080}
+                        height={2080}
+                        className="w-full h-full block group-hover:scale-110 transition object-cover object-center"
+                      />
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                  <span
+                    className="break-all px-2 text-sm pt-4"
+                    dangerouslySetInnerHTML={{ __html: desc.replaceAll(',', '<br />') }}
+                  />
+                  <div className="flex flex-col items-end px-2 gap-1 pb-4 text-xs">
+                    <span>{locale}</span>
+                    <span className="bg-gray-300 w-fit p-1 px-4 rounded-full">
+                      {postalCode}
+                    </span>
                   </div>
-                ) : (
-                  <></>
-                )}
-                <span
-                  className="break-all px-2 text-sm pt-4"
-                  dangerouslySetInnerHTML={{ __html: desc.replaceAll(',', '<br />') }}
-                />
-                <div className="flex flex-col items-end px-2 gap-1 pb-4 text-xs">
-                  <span>{locale}</span>
-                  <span className="bg-gray-300 w-fit p-1 px-4 rounded-full">
-                    {postalCode}
-                  </span>
                 </div>
-              </div>
-            ))}
+              ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
