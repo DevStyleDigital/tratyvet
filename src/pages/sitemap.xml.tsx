8e4e5c38e76eaa97a503ad1e90b4f6dc8270f246
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2024-04-05 19:08:08 -0300

    add index search

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -7,36 +7,26 @@ const URL = 'https://tratyvet.com.br';
 
 function generateSiteMap(ids: string[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-      xmlns:xhtml="http://www.w3.org/1999/xhtml"
-      xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
-      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-      xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-
+    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
       ${['', ...Object.keys(locales)].map(
         (lang) => `
           <url>
-            <loc>https://tratyvet.com.br${lang === '' ? '/' : `/${lang}`}</loc>
+            <loc>${URL}${lang === '' ? '/' : `/${lang}`}</loc>
             <lastmod>2024-04-05T14:00:12.405Z</lastmod>
             <priority>1</priority>
           </url>
           <url>
-            <loc>https://tratyvet.com.br${
-              lang === '' ? '/' : `/${lang}/`
-            }distributor</loc>
+            <loc>${URL}${lang === '' ? '/' : `/${lang}/`}distributor</loc>
             <lastmod>2024-04-05T14:00:12.405Z</lastmod>
             <priority>0.9</priority>
           </url>
           <url>
-            <loc>https://tratyvet.com.br${
-              lang === '' ? '/' : `/${lang}`
-            }becomes-distributor</loc>
+            <loc>${URL}${lang === '' ? '/' : `/${lang}/`}becomes-distributor</loc>
             <lastmod>2024-04-05T14:00:12.405Z</lastmod>
             <priority>0.9</priority>
           </url>
           <url>
-            <loc>https://tratyvet.com.br${lang === '' ? '/' : `/${lang}/`}products</loc>
+            <loc>${URL}${lang === '' ? '/' : `/${lang}/`}products</loc>
             <lastmod>2024-04-05T14:00:12.405Z</lastmod>
             <priority>0.9</priority>
           </url>
@@ -44,9 +34,7 @@ function generateSiteMap(ids: string[]) {
             .map((id) => {
               return `
                 <url>
-                  <loc>${`https://tratyvet.com.br${
-                    lang === '' ? '/' : `/${lang}/`
-                  }products/${id}`}</loc>
+                  <loc>${`${URL}${lang === '' ? '/' : `/${lang}/`}products/${id}`}</loc>
                   <priority>0.8</priority>
                 </url>
               `;
